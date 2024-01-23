86224cfa0d13bf1c0ecc82b953a67442d6dea10c
SreehariJiji <sjiji@flemingcollege.ca>
2024-01-23 09:30:20 -0500

    Created complex data structure

---
+++ script.py
@@ -0,0 +1,36 @@
+def main():
+
+    # TODO: Created a complex data structure
+    about_me = {'full_name' : 'Sreehari Jiji',
+                'student_id': '10316483',
+                'pizza_toppings': ['BACON','PEPPORONI','SAUSAGE'] ,
+                'movies': [{'title': 'rocky', 'genre': 'Thriller'}] ,
+                          [{'title': 'rocky 2', 'genre': 'Thriller'}]
+                }
+
+    # TODO: Step 3 - Add another movie to the data structure
+    
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    print(str("Sreehari Jiji"))
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    print()
+    return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    return
+    
+if __name__ == '__main__':
+    main()
