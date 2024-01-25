9b0dca16ea64aadcbb6763fd244501470df52a59
SreehariJiji <sjiji@flemingcollege.ca>
2024-01-25 03:54:21 -0500

    Final output

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ def main():
                 {'title': 'django unchained', 'genre': 'action'}]
                 }
 
-    # Step 3 - Added another movie to the data structure
+    # Added another movie to the data structure
     about_me['movies'].append({'title': 'shrek 2', 'genre': 'comedy'})
     
     # printing student name and id
@@ -26,7 +26,7 @@ def main():
     print_movie_titles(about_me['movies'])
 
     
-# Step 4 - Created Function that prints student name and ID	
+# Created Function that prints student name and ID	
 def print_student_name_and_id (about_me):
 
     full_name = about_me['full_name']
@@ -53,7 +53,7 @@ def CAPITAL_print_pizza_toppings(about_me):
 
     
     
-# Step 5 - Created Function that adds pizza toppings to data structure
+# Created Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
 
     about_me['pizza_toppings'].extend(toppings)
@@ -63,7 +63,7 @@ def add_pizza_toppings(about_me, toppings):
     return
 
 
-#Step 6 - Created Function that prints bullet list of pizza toppings
+# Created Function that prints bullet list of pizza toppings
 def print_pizza_toppings(about_me):
 
     print("My favourite pizza toppings are:")
@@ -75,7 +75,7 @@ def print_pizza_toppings(about_me):
 
 
 
-#Step 7 - Created Function that prints comma-separated list of movie genres
+# Created Function that prints comma-separated list of movie genres
 def print_movie_genres(about_me):
 
     genres = [movies['genre'] for movies in about_me['movies']]
@@ -91,7 +91,7 @@ def print_movie_genres(about_me):
     return 
 
 
-# Step 8 - Created Function that prints comma-separated list of movie titles
+# Created Function that prints comma-separated list of movie titles
 def print_movie_titles(movie_list):
 
     titles = [movie['title'].title() for movie in movie_list]
